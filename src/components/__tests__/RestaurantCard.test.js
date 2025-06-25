def30815556f9ff6ae506551ad64e5250e0ad1ab
Nikita_chakre <chakrenikita1234@gmail.com>
2025-06-25 14:02:26 +0530

    CORS issue resolved

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,4 +1,4 @@
-import RestaurantCard from "../RestaurantCard"
+import RestaurantCard, { withPromotedLabel } from "../RestaurantCard"
 import { render , screen} from "@testing-library/react"
 import MOCK_DATA from "../mocks/resCardData.json"
 import "@testing-library/jest-dom"
@@ -9,4 +9,5 @@ it("Should render RestuarnatsCard in component with prop Data", () => {
     render(<RestaurantCard resData={MOCK_DATA}/>);
     const name = screen.getByText("Gurukripa Restaurant - Sarwate");
     expect(name).toBeInTheDocument();
-})
+});
+
